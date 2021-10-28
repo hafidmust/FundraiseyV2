50ffd837f61b5189e76993491a9355b53def36ca
Indiana <daradwitanty@gmail.com>
2021-10-28 13:50:48 +0700

    Add splashscreen, onboarding, and update MainActivity

--- app/src/main/java/app/binar/synrgy/android/finalproject/MainActivity.kt
+++ app/src/main/java/app/binar/synrgy/android/finalproject/MainActivity.kt
@@ -1,11 +1,30 @@
 package app.binar.synrgy.android.finalproject
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import app.binar.synrgy.android.finalproject.constant.Const
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPreferences: SharedPreferences =
+            applicationContext.getSharedPreferences(Const.PREF_NAME, Context.MODE_PRIVATE)
+
+    Handler(Looper.getMainLooper()).postDelayed({
+        if (sharedPreferences.getBoolean(Const.IS_LOGIN, false)) {
+            // startActivity -> HomeNavigation
+        } else {
+            // startActivity -> SignIn
+        }
+    }, 5000)
     }
+
 }
---
+++ app/src/main/java/app/binar/synrgy/android/finalproject/constant/Const.kt
@@ -0,0 +1,10 @@
+package app.binar.synrgy.android.finalproject.constant
+
+object Const {
+    const val USER_ID = "USER_ID"
+    const val PREF_NAME = "MY_DOCTOR"
+    const val EMAIL = "EMAIL"
+    const val PASSWORD = "PASSWORD"
+
+    const val IS_LOGIN = "IS_LOGIN"
+}
