f32aaf9d753841b8c50c00968fd40f30442942b6
Indiana <daradwitanty@gmail.com>
2021-11-01 10:29:50 +0700

    rebase string values

---
+++ app/src/main/java/app/binar/synrgy/android/finalproject/ui/onboarding/MainActivity.kt
@@ -0,0 +1,14 @@
+package app.binar.synrgy.android.finalproject.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.binar.synrgy.android.finalproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+    }
+}
