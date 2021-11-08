cab1a9ebf9cb1fd9d37edd0748c11d13198acbea
Indiana <daradwitanty@gmail.com>
2021-11-08 19:22:38 +0700

    Fix onboarding and viewpageradapter

--- app/src/main/java/app/binar/synrgy/android/finalproject/ui/onboarding/MainActivity.kt
+++ app/src/main/java/app/binar/synrgy/android/finalproject/ui/onboarding/MainActivity.kt
@@ -1,14 +1,22 @@
 package app.binar.synrgy.android.finalproject.ui.onboarding
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import app.binar.synrgy.android.finalproject.R
+import app.binar.synrgy.android.finalproject.constant.Const
+import app.binar.synrgy.android.finalproject.ui.homenavigation.HomeNavigationActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportActionBar?.hide()
+        // NOTE : MainActivity -> SplashScreen
+
     }
 }
---
+++ app/src/main/java/app/binar/synrgy/android/finalproject/ui/onboarding/SlideActivity.java
@@ -0,0 +1,56 @@
+package app.binar.synrgy.android.finalproject.ui.onboarding;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import app.binar.synrgy.android.finalproject.R;
+import app.binar.synrgy.android.finalproject.ui.homenavigation.HomeNavigationActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class SlideActivity extends AppCompatActivity {
+
+    public static ViewPager viewPager;
+    SlideViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide);
+
+        viewPager = findViewById(R.id.viewpager);
+        adapter = new SlideViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        SharedPreferences.Editor editor = getSharedPreferences("slide", MODE_PRIVATE).edit();
+        editor.putBoolean("slide", true);
+        editor.commit();
+
+////        if (AppHasOpened())
+////        {
+////            Intent intent=new Intent(SlideActivity.this, MainActivity.class);
+////            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+////            startActivity(intent);
+////        }
+////        else
+////        {
+//            SharedPreferences.Editor editor=getSharedPreferences("slide",MODE_PRIVATE).edit();
+//            editor.putBoolean("slide",true);
+//            editor.commit();
+////        }
+    }
+
+//    private boolean AppHasOpened() {
+//
+//        SharedPreferences sharedPreferences=getSharedPreferences("slide",MODE_PRIVATE);
+//        boolean result=sharedPreferences.getBoolean("slide",false);
+//        return result;
+//
+//    }
+}
