06a15cfd1a80e7ef61770b7c933ba6f44f4e8742
Indiana <daradwitanty@gmail.com>
2021-11-22 19:40:09 +0700

    Add HomeFragment and Loan Screen

---
+++ app/src/main/java/app/binar/synrgy/android/finalproject/ui/homenavigation/home/HomeActivity.kt
@@ -0,0 +1,22 @@
+package app.binar.synrgy.android.finalproject.ui.homenavigation.home
+
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.binar.synrgy.android.finalproject.R
+import app.binar.synrgy.android.finalproject.databinding.FragmentHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_home)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
