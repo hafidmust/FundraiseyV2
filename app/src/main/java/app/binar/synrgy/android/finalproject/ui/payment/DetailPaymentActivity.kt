235798dd4b7e2a00d0055fe9aae925ad71437e1b
Hafid Ali Mustaqim <hafidalimustaqim13@gmail.com>
2021-11-23 20:53:43 +0700

    add feature copy Virtual Account to clipboard

--- app/src/main/java/app/binar/synrgy/android/finalproject/ui/payment/DetailPaymentActivity.kt
+++ app/src/main/java/app/binar/synrgy/android/finalproject/ui/payment/DetailPaymentActivity.kt
@@ -1,7 +1,11 @@
 package app.binar.synrgy.android.finalproject.ui.payment
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import app.binar.synrgy.android.finalproject.R
 import app.binar.synrgy.android.finalproject.databinding.ActivityDetailPaymentBinding
 
@@ -25,6 +29,19 @@ class DetailPaymentActivity : AppCompatActivity() {
             adapterPaymentGuide.update(it)
         })
 
+        binding.copy.setOnClickListener {
+            copyVAN()
+        }
 
+
+    }
+
+    private fun copyVAN() {
+        val tvToCopy = binding.tvvirtualnumber.text.toString()
+        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipData = ClipData.newPlainText("text", tvToCopy)
+        clipboardManager.setPrimaryClip(clipData)
+
+        Toast.makeText(this, "Copied to clipboard",Toast.LENGTH_SHORT).show()
     }
 }
