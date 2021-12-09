0e8ae34c2c9061f4d36a3a9fb4b5e0fc846f3f0d
Indiana <daradwitanty@gmail.com>
2021-12-09 10:40:47 +0700

    Completed InformationFragment: - Button responsive - Content FAQ Added - Layout Adjusted

--- app/src/main/java/app/binar/synrgy/android/finalproject/ui/homenavigation/information/InformationFragment.kt
+++ app/src/main/java/app/binar/synrgy/android/finalproject/ui/homenavigation/information/InformationFragment.kt
@@ -5,31 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import app.binar.synrgy.android.finalproject.R
-import app.binar.synrgy.android.finalproject.databinding.FragmentHomeBinding
 import app.binar.synrgy.android.finalproject.databinding.FragmentInformationBinding
 import android.content.Intent
 import android.net.Uri
+import app.binar.synrgy.android.finalproject.data.information.InformationResponse
 
 
 class InformationFragment : Fragment() {
-    private lateinit var informationViewModel: InformationViewModel
-    private var _binding: FragmentInformationBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var viewModel: InformationViewModel
+    private lateinit var binding: FragmentInformationBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    )
-    : View? {
-        informationViewModel =
-            ViewModelProvider(this).get(InformationViewModel::class.java)
-
-        _binding = FragmentInformationBinding.inflate(inflater, container, false)
+    ): View? {
+        binding = FragmentInformationBinding.inflate(inflater, container, false)
+        viewModel = InformationViewModel()
 
         val root: View = binding.root
+        val informationAdapter = AdapterInformation(listOf(),
+            object : AdapterInformation.EventListener {
+                override fun click(item: InformationResponse) {
+                }
+            }
+        )
+        val informationAdapter2 = AdapterInformation(listOf(),
+            object : AdapterInformation.EventListener {
+                override fun click(item: InformationResponse) {
+                }
+            }
+        )
+        binding.recyclerFaq1.adapter = informationAdapter
+        binding.recyclerFaq2.adapter = informationAdapter2
         binding.buttonContactFaq.setOnClickListener {
             val number = "+62 82362035167"
             val url = "https://api.whatsapp.com/send?phone=$number"
@@ -37,6 +45,15 @@ class InformationFragment : Fragment() {
             i.data = Uri.parse(url)
             startActivity(i)
         }
+
+        viewModel.onViewLoaded()
+        viewModel.responseFAQ1.observe(viewLifecycleOwner, {
+            informationAdapter.update(it)
+        })
+        viewModel.responseFAQ2.observe(viewLifecycleOwner, {
+            informationAdapter2.update(it)
+        })
+
         return root
     }
 }
